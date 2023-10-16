1476d49f7a1c984b8f0707e5c92f44f2d7b2ca38
Firrizq <firrizq@gmail.com>
2023-10-16 08:27:19 +0700

    setting up gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    //Moshi kotlin codegen
+    id("com.google.devtools.ksp")
+    // Kotlin Parcelize - Parcleable data class
+    id("kotlin-parcelize")
 }
 
 android {
@@ -9,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.firrizq.myapplication"
-        minSdk = 24
+        minSdk = 33
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -31,7 +35,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -44,4 +48,23 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // SplashScreen android 12
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    // Picasso - Image Loader
+    implementation("com.squareup.picasso:picasso:2.8")
+
+    // Moshi
+    // Json Converter - serialize and deserialize objects to and from JSON
+    // 1 of libraries JSON converters for data serialization
+    implementation("com.squareup.moshi:moshi:1.14.0")
+    // kotlin codegen
+    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.14.0")
+
+    // Retrofit - Request HTTP Web Service
+    // HTTP Client - Retrofit with OkHTTP
+    // to retrieve and upload JSON (or other structured data) via a REST based webservice
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
 }
